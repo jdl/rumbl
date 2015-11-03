316f7726dee4d779ac00d489ab89d3f8c75c182a
JDL <jdl@workingasintended.com>
2015-11-03 14:19:20 -0600

    annotations are persisted, page 161

--- web/views/user_view.ex
+++ web/views/user_view.ex
@@ -6,6 +6,10 @@ defmodule Rumbl.UserView do
     "You failed, sir"
   end
 
+  def render("user.json", %{user: user}) do
+    %{id: user.id, username: user.username}
+  end
+
   def first_name(%User{name: name}) do
     name
     |> String.split(" ")
